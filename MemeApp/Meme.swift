6377e4b58c1d3570c792121969f651bab9757f9c
SUN Ka Meng Isaac <skm.isaac@gmail.com>
2015-06-21 10:42:05 +0800

    First Draft undone: activity controller text style collectionView

---
+++ MemeApp/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeApp
+//
+//  Created by SUN Ka Meng Isaac on 20/6/15.
+//  Copyright (c) 2015 SUN Ka Meng Isaac. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
